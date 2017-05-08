463322211fbb059793a461972bc4202e917ded09
Tatu Mäkelä <tatu.makela@solinor.com>
2017-05-08 20:24:50 +0300

    fixed put

--- src/main/java/com/todo/controller/TodoController.java
+++ src/main/java/com/todo/controller/TodoController.java
@@ -16,35 +16,46 @@ import com.todo.repository.TodoRepository;
 @RestController
 @RequestMapping("/todo")
 public class TodoController {
-	
+
 	@Autowired
 	TodoRepository todoRepository;
 
 	//get one todo based on its id: eg. http://localhost:8080/todo/8
-		@CrossOrigin
-		@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-		public Todo getTodo(@PathVariable Long id) throws Exception {
-			return todoRepository.getOne(id);
-		}
+	@CrossOrigin
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Todo getTodo(@PathVariable Long id) throws Exception {
+		return todoRepository.getOne(id);
+	}
 
-		//get all todos eg. http://localhost:8080/todo/
-		@CrossOrigin
-		@RequestMapping(value = "/", method = RequestMethod.GET)
-		public List<Todo> getAllTodos() throws Exception {
-			return todoRepository.findAll();	
-		}
+	//get all todos eg. http://localhost:8080/todo/
+	@CrossOrigin
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public List<Todo> getAllTodos() throws Exception {
+		return todoRepository.findAll();	
+	}
 
-		//create a new todo http://localhost:8080/todo/
-		@CrossOrigin
-		@RequestMapping(value = "/", method = RequestMethod.POST, headers = {"Content-type=application/json"})
-		public Todo newTodo(@RequestBody Todo t) throws Exception {
-			return todoRepository.save(t);
-		}
+	//create a new todo http://localhost:8080/todo/
+	@CrossOrigin
+	@RequestMapping(value = "/", method = RequestMethod.POST, headers = {"Content-type=application/json"})
+	public Todo newTodo(@RequestBody Todo t) throws Exception {
+		return todoRepository.save(t);
+	}
 
-		//delete todo by id. http://localhost:8080/todo/8
-		@CrossOrigin
-		@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-		public void deleteTodo(@PathVariable Long id) {
-			todoRepository.delete(id);
-		}
+	//update a todo http://localhost:8080/todo/
+	@CrossOrigin
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, headers = {"Content-type=application/json"})
+	public Todo updateTodo(@PathVariable Long id, @RequestBody String t) throws Exception {
+		Todo todo = todoRepository.getOne(id);
+		
+			todo.setTaskName(t);
+		
+		return todoRepository.save(todo);
+	}
+
+	//delete todo by id. http://localhost:8080/todo/8
+	@CrossOrigin
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void deleteTodo(@PathVariable Long id) {
+		todoRepository.delete(id);
+	}
 }
